f114b4806331922d7d27a58ea918f38cdbec986c
AlexanderSoeyland <alexahas@stud.ntnu.no>
2026-09-24 16:01:51 +0200

    Made my workfolder

---
+++ Alexander_work_folder/EDA.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy import stats
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+# Plot settings
+import seaborn as sns
+
+
+# 1 Make a clean data function 
+
+# 2 Make a split data function into training, validation and test sets 
+
+# 
